7e83b887700db5fac5489660f49f0c71b91ca4fd
qbhy <96qbhy@gmail.com>
2024-03-15 00:19:33 +0400

    fix: 修复ssehandler问题

--- sse/handler.go
+++ sse/handler.go
@@ -13,12 +13,12 @@ func New(path string, controller contracts.SseController) (string, any) {
 	factory := application.Get("sse.factory").(contracts.SseFactory)
 	factory.Register(path, NewSse())
 
-	return path, func(request *http.Request, serializer contracts.Serializer, sseFactory contracts.SseFactory) {
+	return path, func(request *http.Request, serializer contracts.Serializer, sseFactory contracts.SseFactory) any {
 		sse := sseFactory.Sse(path)
 		var fd = sse.GetFd()
 		if err := controller.OnConnect(request, fd); err != nil {
 			logs.WithError(err).WithFields(request.Fields()).WithField("fd", fd).Debug("sse.NewRouter: OnConnect failed")
-			return
+			return nil
 		}
 
 		request.Request.SetContentType("text/event-stream")
@@ -74,6 +74,7 @@ func New(path string, controller contracts.SseController) (string, any) {
 				}
 			}
 		})
+		return nil
 	}
 }
 
